53545e9c9674763e20262c35f7726e3fd61bb025
Mathéo <matheogirard@hotmail.com>
2024-06-07 20:01:28 +0200

    feat: can create event

---
+++ project_php/src/Controller/EventController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Event;
+use App\Form\EventType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class EventController extends AbstractController
+{
+    #[Route('/create_event', name: 'create_event')]
+    public function createEvent(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($event);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_index');
+        }
+
+        return $this->render('create.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+}
