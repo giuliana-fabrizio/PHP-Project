62ff69a0c4884c9dd91a4719cc374e73aece789f
Giuliana Fabrizio <godailfabriziogiuliana@gmail.com>
2024-06-09 14:54:01 +0200

    feat: can create user

--- project_php/src/Form/UserType.php
+++ project_php/src/Form/UserType.php
@@ -2,23 +2,67 @@
 
 namespace App\Form;
 
+use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('field_name')
+            ->add('nom', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('prenom', TextType::class, [
+                'label' => 'Prénom',
+                'constraints' => [
+                    new NotBlank(['message' => 'Saisir un prénom']),
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+                'attr' => ['class' => 'form-control'],
+                'constraints' => [
+                    new NotBlank(['message' => 'Saisir un email']),
+                    new Email(['message' => 'Veuillez saisir un email valide']),
+                ]
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'first_options' => [
+                    'constraints' => [
+                        new NotBlank(['message' => 'Saisir un mot de passe']),
+                        new Length(['min' => 8, 'minMessage' => 'Votre mot de passe doit avoir une longueur minimale de {{ limit }} caractères.']),
+                    ]
+                ],
+                'second_options' => [
+                    'constraints' => [
+                        new NotBlank(['message' => 'Confirmer votre mot de passe']),
+                    ]
+                ],
+                'invalid_message' => 'Les mots de passe doivent être les mêmes.',
+            ])
+            ->add('save', SubmitType::class, [
+                'label' => 'Créer'
+            ]);
         ;
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            // Configure your form options here
+            'data_class' => User::class,
+            'csrf_protection' => true
         ]);
     }
 }
